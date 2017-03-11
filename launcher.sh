fd8b85cfee050b3408adbb0f08858e62e24fdda0
liuj05 <liuj05@goldmail.etsu.edu>
2017-03-11 15:54:30 -0600

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -12,6 +12,9 @@ BINDIR=/home/012/j/jx/jxl167130/Project/bin
 # Your main project class
 PROG=Proj2
 
+#Path of Config file
+CONFIG=/home/012/j/jxl167130/config.txt
+
 n=0
 
 cat $CONFIGLOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
@@ -19,20 +22,14 @@ cat $CONFIGLOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
     read i
     echo $i
     Node_num=$( echo $i | awk '{print $1}')
-    minPerActive=$( echo $i | awk '{print $2}')
-    maxPerActive=$( echo $i | awk '{print $3}')
-    minSendDelay=$( echo $i | awk '{print $4}')
-    snapshotDelay=$( echo $i | awk '{print $5}')
-    maxNumber=$(echo $i | awk '{print $6}')
-    echo $Node_num
-    echo $minPerActive
     while [ $n -lt $Node_num ]
     do
         read line
         n=$( echo $line | awk '{ print $1 }' )
         host=$( echo $line | awk '{ print $2 }' )
+        port=$( echo $lin4 | awk '{ print $3 }' )
 
-        gnome-terminal -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host java -cp $BINDIR $PROG $n  $Node_num $minPerActive $maxPerActive $minSendDelay $snapshotDelay $maxNumber $CONFIGLOCA; $SHELL" &
+        gnome-terminal -e "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host java -cp $BINDIR $PROG $n $port $CONFIG; $SHELL" &
 
         n=$(( n + 1 ))
     done
