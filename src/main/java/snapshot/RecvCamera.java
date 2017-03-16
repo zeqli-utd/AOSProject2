f2f54f5e4be45404add5c870c7bb95523d69e332
PepperGo <meetmark.sun@gmail.com>
2017-03-16 14:20:36 -0500

    If else of HandleMessage()

--- src/main/java/snapshot/RecvCamera.java
+++ src/main/java/snapshot/RecvCamera.java
@@ -50,21 +50,22 @@ public class RecvCamera extends Process implements Camera {
     // the method handleMessage gives the rule for receiving a marker message
     public void handleMessage(Message m, int srcId, Tag tag) throws IOException {  
         if(tag.equals(Tag.MARKER)){      
-            if(myColor == WHITE)    // if the process is white, it turns red by involking globalState()
-                globalState();
-            closed[srcId] = true;    //set closed[srcId] to true because there cannot be any message of type wr in that channel after the marker is received
-            if(isDone()){  
-                System.out.println("Channel State : Transit Messages");
-                for(int i = 0; i < numProc; i++){
-                    while(!channels.get(i).isEmpty()){
-                        System.out.println(channels.get(i).remove(0).toString());
-                    }
-                }
-            } else { // handle application message, true if the application message is of type wr
-                if(myColor == RED && !closed[srcId])
-                    channels.get(srcId).add(m);
-                app.handleMessage(m, srcId, tag);
-            }
+        	 if(myColor == WHITE)    // if the process is white, it turns red by involking globalState()
+                 globalState();
+             closed[srcId] = true;    //set closed[srcId] to true because there cannot be any message of type wr in that channel after the marker is received
+             if(isDone()){  
+                 System.out.println("Channel State : Transit Messages");
+                 for(int i = 0; i < numProc; i++){
+                     while(!channels.get(i).isEmpty()){
+                         System.out.println(channels.get(i).remove(0).toString());
+                     }
+                 }
+             } 
+        }
+        else { // handle application message, true if the application message is of type wr
+            if(myColor == RED && !closed[srcId])
+                channels.get(srcId).add(m);
+            app.handleMessage(m, srcId, tag);
         }
 
     }
