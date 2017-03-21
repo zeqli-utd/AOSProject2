dde4e480c1af353d5a57f3207c6aa8a45bb3a9e9
Zeqli <l.caecar@gmail.com>
2017-03-21 11:07:17 -0500

    Fixed verification script

--- grade.py
+++ grade.py
@@ -2,54 +2,90 @@
 
 import sys
 
-def HappensBefore(C_e, i, C_f, j):
-	if i == j:
-		if C_e[i] < C_f[i]:
-			return True
-	else:
-		if C_e[i] <= C_f[i]:
-			return True
-	return False;
+class Vector:
+	def __init__(self, i, vector):
+		self.pid = i
+		self.v = vector
+		self.vLen = len(vector)
+		
+		
+	def getValue(self, i):
+		return self.v[i]
+
+class GlobalState:
+	'A collection of local state'
+	
+	def __init__(self, n):
+		self.numProc = n;
+		self.G = []
+		
+	# Push a vector into global state
+	def push(self, vClock):
+		if isinstance(vClock, Vector):
+			if vClock.vLen != self.numProc:
+				raise IndexError('Vector size incomp')
+			if len(self.G) >= self.numProc:
+				raise IndexError('GlobalState is full!')
+				
+			self.G.append(vClock)
+		else:
+			raise TypeError('Not a vector')
+	
+	def isConsistent(self):
+		vMax = [0 for i in range(self.numProc)]
+		for vClock in self.G:
+			v = vClock.v
+			for i in range(self.numProc):
+				vMax[i] = max(vMax[i], v[i])
+		for i in range(self.numProc):
+			if self.G[i].getValue(i) != vMax[i]:
+				print('Value {:d} in V_max {:s} != Value {:d} in G[i] {:s}'.format(int(vMax[i]), \
+				str(vMax), self.G[i].getValue(i), str(self.G[i].v)))
+				return False
+		return True
+
+v1 = Vector(0, [1, 1])
+v2 = Vector(1, [0, 0])
+G = GlobalState(2)
+G.push(v1)
+G.push(v2)
+print(G.isConsistent())
+
 
 print('Number of arguments:',len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
 print('Number of ndoes', sys.argv[1])
 n = int(sys.argv[1])
+globalStateCollection = []
+files = []
 
-
-matrix = []
-
-path = "config-0.out";
-print(path)
-with open(path, 'r') as f:
-	for j, line in enumerate(f):
-		row = [];
-		row.append([int(i) for i in line.split()])
-		matrix.append(row)	
-	
-out = []
-for i in range (1, n):
-	path = "config-" + str(i) +".out";
-	print(path)
-	out.append(path)
+for i in range(n):
+	path = 'config-' + str(i) + '.out'
 	with open(path, 'r') as f:
 		for j, line in enumerate(f):
-			matrix[j].append([int(i) for i in line.split()])			
+			vClock = Vector(i, [int(k) for k in line.split()])
+			if j >= len(globalStateCollection):
+				G = GlobalState(n)  # New GlobalState
+				G.push(vClock)
+				globalStateCollection.append(G)
+			else:
+				G = globalStateCollection[j]
+				G.push(vClock)
 
-for row in matrix:
-	print(row)
-	isConsistent = True;
-	for i in range( len(row)):
-		for j in range (i + 1, len(row)):
-			if (HappensBefore(row[i], i, row[j], j)):
-				isConsistent = False;
-				print(str(row[i]) + " " + str(i) + " Happens Before " + str(row[j]) + " " + str(j))
-			if (HappensBefore(row[j], j, row[i], i)):
-				isConsistent = False;
-				print(str(row[j]) + " " + str(j) + " Happens Before " + str(row[i]) + " " + str(i))
-	print(isConsistent)
-			
 
+				
+for i in range(len(globalStateCollection)):
+	G = globalStateCollection[i]
+	print("Snapshot " + str(i) + " Consistency: ", end='')
+	print(G.isConsistent())
+	count = 0
+	for vClock in G.G:
+		print(' ' + str(count) + ' ' + str(vClock.v))
+		count += 1
+	print()
 
 	
+			
 
+	
+	
